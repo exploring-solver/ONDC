906616195fd5fe33f2ed74c3679948a4e2e08896
exploring-solver <amansharma12607@gmail.com>
2024-02-08 18:58:06 +0530

    life is boring

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('get/', views.GetCatalogues.as_view(), name='get_catalouges'),
     path('create/', views.CreateCatalogue.as_view(), name='create_catalouge'),
     path('get-all/', views.GetAllCatalogues.as_view(), name='get_all_catalouges'),
+    path('categories/', views.get_all_categories, name='get_all_categories'),
 ]
+
--- catalogue/views.py
+++ catalogue/views.py
@@ -5,6 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import JsonResponse
+from .models import Categories
 # Create your views here.
 
 
@@ -68,7 +70,7 @@ class CreateCatalogue(APIView):
         else:
             return Response(catalogue_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-# to get all catalogues even without loggin in    
+# to get all catalogues even without logging in    
 class GetAllCatalogues(APIView):
     def get(self, request):
         all_catalogues = Catalogue.objects.all()
@@ -92,3 +94,6 @@ class GetAllCatalogues(APIView):
 
         return Response(data, status=status.HTTP_200_OK)
         
+def get_all_categories(request):
+    categories = Categories.objects.all().values_list('category', flat=True)
+    return JsonResponse({'categories': list(categories)})
